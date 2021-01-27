d98413b8887513c7552cc1c223f9576c11c29464
Elogeek <elodiechristin@gmail.com>
2021-01-27 12:43:35 +0100

    fcts math PHP

--- index.php
+++ index.php
@@ -4,22 +4,23 @@
  * 1. Affichez dans un <p> la valeur absolue de -8
  */
 $value = -8;
-// TODO Votre code ici.
 
+// TODO Votre code ici.
+echo abs(-8) ."<p></p>";
 
 /**
  * 2. Affichez dans un <p> la valeur de 5.64 arrondit au nombre supérieur.
  */
 $value = 5.64;
 // TODO Votre code ici.
-
+echo ceil(5.64) ."<p></p>";
 
 /**
  * 3. Affichez dans un <p> la valeur de 5.34 arrondit au nombre inférieur.
  */
 $value = 5.34;
 // TODO Votre code ici.
-
+echo floor(5.34)."<p></p>";
 
 /**
  * 4. Affichez dans un <p> la valeur maximum du tableau $max, et la valeur minimum du tableau $min.
@@ -27,14 +28,17 @@ $value = 5.34;
 $max = [5, 10, 12, 8, 7, 6, 54, 39, 76, 63, 84, 2, 21];
 $min = [5, 41, 65, 4, 3, 65, 2, 65, 74, 51, 23, 39, 42];
 // TODO Votre code ici.
-
+echo max($max)."<p></p>";
+echo min($min)."<p></p>";
 
 /**
  * 5. Affichez dans un <p> la valeur de PI * 2 ( avec une fonction mathématique pour PI ).
  */
 
 // TODO Votre code ici.
+echo pow(pi(),2)."<p></p>";
 
+echo pi()*2 ."<p></p>";
 
 /**
  * 6. Affichez dans un <p> la valeur arrondie de 5.42 avec la fonction round().
@@ -42,7 +46,7 @@ $min = [5, 41, 65, 4, 3, 65, 2, 65, 74, 51, 23, 39, 42];
 $value = 5.42;
 // TODO Votre code ici.
 
-
+echo round(5.42)."<p></p>";
 
 
 
